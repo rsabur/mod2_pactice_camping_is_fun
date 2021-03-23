16d20ed4114cf0b61c1ef998a7dc673114dfe4d1
Radiyah <rsabur58@gmail.com>
2021-03-23 16:28:45 -0400

    Done.

--- app/models/signup.rb
+++ app/models/signup.rb
@@ -2,5 +2,5 @@ class Signup < ApplicationRecord
     belongs_to :activity
     belongs_to :camper
 
-    validates :time, inclusion: {in: 0..24, message: 'The time must be between 0 and 24 hours'}
+    validates :time, inclusion: {in: 0..24, message: 'must be between 0 and 24 hours'}
 end
